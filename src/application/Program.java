404a010bb3d74c264f1d91cd6e50f3c6172dd220
ThainaSantoss <thainados_santos@hotmail.com>
2026-05-28 21:16:52 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,13 @@ package application;
 
 
 
-import java.util.Date;
+import java.util.Date; 
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
+
 
 public class Program {
 
@@ -16,6 +19,8 @@ public class Program {
 		
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com",  new Date() , 3000.0, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		
 		System.out.println(seller);
 	}
